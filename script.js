6d39244f30b0ad20e143169e5f42518625b7aec0
yoshisato22 <111165268+yoshisato22@users.noreply.github.com>
2022-11-23 11:32:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function set2fig(num) {
+    var ret;
+    if( num < 10 ) { ret = "0" + num; }
+    else { ret = num; }
+    return ret;
+    }
+    function showClock() {
+    var nowTime = new Date();
+    var nowHour = set2fig( nowTime.getHours() );
+    var nowMin  = set2fig( nowTime.getMinutes() );
+    var nowSec  = set2fig( nowTime.getSeconds() );
+    var msg = nowHour + ":" + nowMin + ":" + nowSec;
+    document.getElementById("RealtimeClock").innerHTML = msg;
+    }
+    setInterval('showClock()',1000);
+
+
+function showDate() {
+    var today = new Date();
+    var date = today.getFullYear() + '/' + (today.getMonth()+1) + '/' + today.getDate();
+    document.getElementById("RealtimeDate").innerHTML = date;
+    }
+    setInterval('showDate()',1000);
